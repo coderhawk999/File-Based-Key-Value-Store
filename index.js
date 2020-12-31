5961e2dfe05b9d8decf1fb783784b3003be9cddc
coderhawk999 <afroz.cs17@sahyadri.edu.in>
2020-12-31 23:38:29 +0530

    added create functionality

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const fs = require('fs')
-var sizeof = require('object-sizeof')
 const os = require('os');
 
-var tempPath = os.homedir()\;
+var tempPath = os.homedir()+ '/tmp/data';
+var dir  = os.homedir()+ '/tmp';
 
-
-var loadFile = (path)=>{
+function isEmptyObject(obj) {
+    for(var prop in obj) {
+      if(obj.hasOwnProperty(prop)) {
+        return false;
+      }
+    }
+  
+    return JSON.stringify(obj) === JSON.stringify({});
+  }
+var loadFile = (filePath)=>{
     try {
-        return fs.readFileSync(path, 'utf8')
+        return JSON.parse(fs.readFileSync(filePath, 'utf8'))
       } catch (err) {
         throw new Error('Invalid file Path or file does not exist ');
       }
@@ -29,18 +37,121 @@ var loadFile = (path)=>{
     }
     else
     {
-        console.log(`File path not provided , file will be created in ${tempPath}`)
+        if (!fs.existsSync(dir)){
+            fs.mkdirSync(dir);
+            try {
+                fs.writeFileSync(tempPath, JSON.stringify({}))
+              } catch (err) {
+                console.error(err) 
+              }
+            console.log(`File path not provided , Data will be stored at ${tempPath}`)
+            return tempPath;
+        }
+        else
+        {
+            return tempPath;
+        }
     }
 }
 
-var Store = function(filePath="")
-{
-    this.data = {};
-    this.filePath = filePathValidation(filePath);
+var keyValidation = (key)=>{
+    
+    if(key == undefined)
+    {
+        throw new Error("Key should be Provided, to create a store");
+    }
+    if(!(typeof key === 'string'))
+    {
+        throw new Error("Invalid Key, should be a String");
+    }
+    if(key.length == 0 || key.length > 32)
+    {
+        console.log("Key Size :",key.length)
+        throw new Error("Valid Key Should be Provided, Key size should greator than zero or less than 32 characters in size");
+    }
 
+    return true
+}
+
+var dataValidation = (data)=>{
+    var value = data;
+    if(!(typeof data === 'object'))
+    {
+        try{
+            value = JSON.parse(data);
+        }
+        catch(err)
+        {
+            throw new Error("Invalid JSON data")
+        }
+    }
+    if(isEmptyObject(value))
+    {
+        throw new Error("Empty JSON data");
+    }
+    const size = Buffer.byteLength(JSON.stringify(value))
+    if(size >16000)
+    {
+        throw new Error("JSON data should be less than 16KB");
+    }
+    
 }
 
 
-// var mystore  = new Store("./test");
+var storeData = (filePath,data)=>{
+    try {
+        fs.writeFileSync(filePath, JSON.stringify(data))
+      } catch (err) {
+        throw new Error("Failed while Saving data, File might have been deleted")
+      }
+}
 
+
+var Store = function(filePath="")
+{
+    this.filePath = filePathValidation(filePath);
+    this.data = loadFile(this.filePath);
+    console.log(this.data)
+
+}
+
+Store.prototype.read = function(key)
+{
+    keyValidation(key)
+}
+
+Store.prototype.create = function(key = "",data = {},timeToLive = 0){
+    // key validation
+     keyValidation(key);
+        // Check if Key Already Exists
+        if(this.data.hasOwnProperty(key))
+        {
+            throw new Error("Key already Exists")   
+        }
+
+     // data validation
+     dataValidation(data)
+     var input_val = [data,0];
+     // time to live validation
+    if(typeof timeToLive!== "number")
+    {
+        throw new Error("Titme to live must be a Number in seconds");
+    }
+     if(timeToLive == 0)
+     {
+         input_val[1] = 0;
+     }
+     else
+     {
+         const now = new Date();
+         input_val[1] = timeToLive + now.getTime();
+     }
+     this.data[key] = input_val;
+     storeData(this.filePath,this.data);
+}
+
+
+var mystore  = new Store();
+
+mystore.create("byebye",{data:"Smthing"},0);
 module.exports  = Store;
